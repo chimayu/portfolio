d26611daea62d18dcf46ce2fefacdf4333537772
mayumi <mayumi@asadigital.net>
2019-10-15 15:17:45 +0900

    vue-fontawesomeのパッケージ導入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,31 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// FontAwesome ここから
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee, faChild, faCircle, faArchive, faPen, faCat } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+import { faComment } from '@fortawesome/free-regular-svg-icons'
+import { faTwitter, faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faCoffee,
+  faChild,
+  faTwitter,
+  faCircle,
+  faArchive,
+  faComment,
+  faGithub,
+  faFacebook,
+  faPen,
+  faCat
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
+// FontAwesome ここまで
+
 Vue.config.productionTip = false
 
 new Vue({
