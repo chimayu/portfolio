5aa0aae2c681df74f79bea0c91f083b2f072aff6
mayumi <mayumi@asadigital.net>
2019-10-15 21:25:55 +0900

    vue.config.js更新

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    publicPath : process.env.NODE_ENV === 'production'
-        ? '/portfolio/'
-        : '/',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/portfolio2/'
+      : '/',
     outputDir: 'docs'
-}
+  }
